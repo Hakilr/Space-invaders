0a6c03f3927950525fb46dad5dd7a84f84cfa9ac
Hakilr <hakilr@hotmail.co.uk>
2025-11-27 18:57:29 +0000

    Empty Game screen created

---
+++ spaceinvaders.py
@@ -0,0 +1,31 @@
+import pygame
+import sys
+
+pygame.init()
+
+# Set up the display
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Space Invaders")
+
+
+COLOUR = (0, 0, 0)
+
+# Main game loop
+running = True
+
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # Fill the screen
+    screen.fill(COLOUR)
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
+
