4ff345f29bb82df1ba26980df93d2d0c4bba8420
Hakilr <hakilr@hotmail.co.uk>
2025-11-27 21:19:08 +0000

    removed ship functions & added to ship.py

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from ship import Ship
 
 pygame.init()
 
@@ -9,26 +10,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Space Invaders")
 
-
 COLOUR = (0, 0, 0)
 
-# Load ship image
-ship_image = pygame.image.load("images/ship.bmp")
-ship_rect = ship_image.get_rect()
-
-# Position ship at the bottom center of the screen
-ship_rect.centerx = SCREEN_WIDTH // 2
-ship_rect.bottom = SCREEN_HEIGHT
-
-def move_ship_left():
-    # Check if ship would go beyond left boundary
-    if ship_rect.left > 0:
-        ship_rect.centerx -= 5
-
-def move_ship_right():
-    # Check if ship would go beyond right boundary
-    if ship_rect.right < SCREEN_WIDTH:
-        ship_rect.centerx += 5
+# Create ship instance
+ship = Ship("images/ship.bmp", SCREEN_WIDTH, SCREEN_HEIGHT)
 
 # Main game loop
 running = True
@@ -43,15 +28,15 @@ while running:
     # continuous movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        move_ship_left()
+        ship.move_left()
     if keys[pygame.K_RIGHT]:
-        move_ship_right()
+        ship.move_right()
     
     # Fill the screen
     screen.fill(COLOUR)
     
     # Draw the ship
-    screen.blit(ship_image, ship_rect)
+    ship.draw(screen)
 
     pygame.display.flip()
     clock.tick(60)
