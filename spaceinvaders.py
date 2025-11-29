82dc49d22c945a6de28d620a4a545d78db1048a4
Hakilr <hakilr@hotmail.co.uk>
2025-11-29 10:02:10 +0000

    added class Space_Invaders

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -2,45 +2,49 @@ import pygame
 import sys
 from ship import Ship
 
-pygame.init()
-
-# Set up the display
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Space Invaders")
-
-COLOUR = (0, 0, 0)
-
-# Create ship instance
-ship = Ship("images/ship.bmp", SCREEN_WIDTH, SCREEN_HEIGHT)
-
-# Main game loop
-running = True
-clock = pygame.time.Clock()
-
-while running:
+class Space_Invaders:
+    def __init__(self):
+        pygame.init()
+        
+        # Set up the display
+        self.SCREEN_WIDTH = 800
+        self.SCREEN_HEIGHT = 600
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        pygame.display.set_caption("Space Invaders")
+        
+        self.COLOUR = (0, 0, 0)
+        
+        # Create ship instance
+        self.ship = Ship("images/ship.bmp", self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
+        
+        self.clock = pygame.time.Clock()
+        self.running = True
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    # continuous movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        ship.move_left()
-    if keys[pygame.K_RIGHT]:
-        ship.move_right()
-    
-    # Fill the screen
-    screen.fill(COLOUR)
-    
-    # Draw the ship
-    ship.draw(screen)
+    def run(self):
+        while self.running:
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            # continuous movement
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                self.ship.move_left()
+            if keys[pygame.K_RIGHT]:
+                self.ship.move_right()
+            
+            # Fill the screen
+            self.screen.fill(self.COLOUR)
+            
+            # Draw the ship
+            self.ship.draw(self.screen)
 
-    pygame.display.flip()
-    clock.tick(60)
+            pygame.display.flip()
+            self.clock.tick(60)
+        
+        pygame.quit()
+        sys.exit()
 
-pygame.quit()
-sys.exit()
-
+si = Space_Invaders()
+si.run()
